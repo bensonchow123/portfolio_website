6b57d31f6444a6c9df970d2b5f8609d914eaf063
Benson Chow <email@bensonc.how>
2024-06-22 22:11:00 +0100

    add hovering effect

--- static/js/home.js
+++ static/js/home.js
@@ -2,22 +2,22 @@ $(function() {
   $(".typed").typed({
     strings: [
       "stat Benson.human<br/>" +
-      "><span class='caret'>$</span> fluent languages: Python, Javascript, HTML, CSS<br/>" +
-      "><span class='caret'>$</span> hobbies: audiophile, 3d printing, machanical keyboards <br/> ^150" +
-      "><span class='caret'>$</span> school: year 13 at <a href='https://www.malverncollege.org.uk/'>Malvern college</a><br/> ^200"
+      "><span class='caret'>$</span> Proficient in: Python, JavaScript, HTML, CSS ^100<br/>" +
+      "><span class='caret'>$</span> Hobbies: Audiophile, 3D Printing, Mechanical Keyboards, coffee making<br/> ^100" +
+      "><span class='caret'>$</span> Education: Year 13 at <a href='https://www.malverncollege.org.uk/'>Malvern College</a><br/>"
     ],
     showCursor: false,
     cursorChar: '_',
     autoInsertCss: true,
     typeSpeed: 0.001,
-    startDelay: 50,
+    startDelay: 10,
     loop: false,
-    onStart: $('.message form').hide(),
-    onStop: $('.message form').show(),
-    onTypingResumed: $('.message form').hide(),
-    onTypingPaused: $('.message form').show(),
-    onComplete: $('.message form').show(),
-    onStringTyped: function(pos, self) {$('.message form').show();},
+    onStart: function() { $('.message form').hide(); },
+    onStop: function() { $('.message form').show(); },
+    onTypingResumed: function() { $('.message form').hide(); },
+    onTypingPaused: function() { $('.message form').show(); },
+    onComplete: function() { $('.message form').show(); },
+    onStringTyped: function() { $('.message form').show(); },
   });
-  $('.message form').hide()
+  $('.message form').hide();
 });
