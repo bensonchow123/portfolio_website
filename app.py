7b2eb74aa537b81af5eda216defd5490573b8052
Benson Chow <email@bensonc.how>
2025-01-07 04:08:27 +0000

    add hypixel mining events

--- app.py
+++ app.py
@@ -113,5 +113,10 @@ def chat_exporter():
     else:
         return "Error: No URL provided"
 
+@app.route("/hypixel_mining_event", methods=['GET'])
+def hypixel_mining_event():
+    return render_template('hypixel_mining_event.html')
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
