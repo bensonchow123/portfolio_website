f191efaa4ad5313e0112ea22859b331b944e7522
benson <bensonistaken@gmail.com>
2025-07-06 18:50:39 +0800

    added resume

--- static/js/home.js
+++ static/js/home.js
@@ -2,7 +2,7 @@ $(function() {
   $(".typed").typed({
     strings: [
       "cat benson.txt<br/>" +
-      "Hobbies: programming, audiophilia, 3D printing, VR, keyboard building<br/> ^100" +
+      "Hobbies: programming, audiophilia, 3D printing, VR, GNU/ Linux enthusiast, hardware tinkering<br/> ^100" +
       "Organisation: Year 1 at <a href='https://www.york.ac.uk/'>University of York</a><br/>" +
       "<span class='user'>benson</span><span class='at'>@</span><span class='path'>my-computer</span><span class='caret'>:~$</span>"
     ],
